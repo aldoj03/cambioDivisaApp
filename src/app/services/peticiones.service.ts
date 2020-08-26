408f310d4649a3552b83696bbbe608a1add25c23
aldo <aldoj03@gmail.com>
2020-08-26 11:52:21 -0400

    redirecciones al login

--- src/app/services/peticiones.service.ts
+++ src/app/services/peticiones.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment';
 import { Storage } from '@ionic/storage';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,8 @@ export class PeticionesService {
   public token;
   constructor(
     private httpClient: HttpClient,
-    private storage: Storage
+    private storage: Storage,
+    private router: Router
   ) {
     this.apiUrl = environment.urlApiWordpress
   }
@@ -29,25 +31,39 @@ export class PeticionesService {
   }
   async configInit() {
     const token = await this.storage.get('apiToken')
-
-    return this.httpClient.post(`${this.apiUrl}/api/configInit`, {token})
+    if (!token) {
+      this.router.navigate(['login'])
+    }
+    return this.httpClient.post(`${this.apiUrl}/api/configInit`, { token })
   }
   async getList() {
-    const token = await this.storage.get('apiToken')
 
-    return this.httpClient.post(`${this.apiUrl}/api/returnRegistro`, {token})
+    const token = await this.storage.get('apiToken')
+    if (!token) {
+      this.router.navigate(['login'])
+    }
+    return this.httpClient.post(`${this.apiUrl}/api/returnRegistro`, { token })
   }
 
   async getLastRegistros() {
     const token = await this.storage.get('apiToken')
+    if (!token) {
+      this.router.navigate(['login'])
+    }
     return this.httpClient.post(`${this.apiUrl}/api/campareCambio`, { token })
   }
   async updateCambios(cambios) {
     const token = await this.storage.get('apiToken')
+    if (!token) {
+      this.router.navigate(['login'])
+    }
     return this.httpClient.post(`${this.apiUrl}/api/changes`, { ...cambios, token })
   }
   async setConfig(checks) {
     const token = await this.storage.get('apiToken')
+    if (!token) {
+      this.router.navigate(['login'])
+    }
 
     const data = { ...checks, token };
 
